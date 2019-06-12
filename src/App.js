97b35187c9df95890e422c1b7c37e4b955390c17
Dave Calderwood <43446756+davecalderwood@users.noreply.github.com>
2019-06-12 18:45:50 -0400

    changed app page to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './app.scss'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './pages/Home'
@@ -6,35 +6,48 @@ import Finance from './pages/Finance'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-export default function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-      
-    <div className="mainApp">
-      <Router>
-        <div className="links">
-          <Link to="/">
-                <div className="home">
-                    <h3>Home</h3>
-                </div>
-              </Link>
+export default class App extends Component {
+  constructor() {
+    super();
+    
+    // Initial state
+    this.state = { open: false };
+    
+  }
 
-            <Link to="/finance">
-              <div className="finance">
-                <h3>Finance</h3>
+  toggle() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+    render() {
+        return(
+          <div className="App">
+          <Header />
+          <Footer />
+        <div className="mainApp">
+          <Router>
+            <div className="links">
+              <Link to="/">
+                    <div className="home">
+                        <h3>Home</h3>
+                    </div>
+                  </Link>
+    
+                <Link to="/finance">
+                  <div className="finance">
+                    <h3>Finance</h3>
+                  </div>
+                </Link>
               </div>
-            </Link>
+    
+              <div className="routes">
+                <Route exact path="/" component={Home} />
+                <Route path="/finance" component={Finance} />
+              </div>
+            </Router>
           </div>
-
-          <div className="routes">
-            <Route exact path="/" component={Home} />
-            <Route path="/finance" component={Finance} />
-          </div>
-        </Router>
-      </div>
-    </div>
-  );
+        </div>          
+        );
+    }
 }
-
